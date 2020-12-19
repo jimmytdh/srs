ca23bdcf949893e3a957ea8cbfafb41c9cb4420e
jimmytdh <jimmy.tdh@gmail.com>
2020-12-19 20:27:05 +0800

    fixed title

--- resources/views/request.blade.php
+++ resources/views/request.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>I.T. Service Requisition Form</title>
+    <title>I.T. Job Request Form</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
